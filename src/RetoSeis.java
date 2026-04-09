f153fa80c5e2f3e665c3f4e008ad697078b195b0
TonaSanchez <tonagsn@gmail.com>
2026-04-09 00:31:06 -0600

    Se realizan dos otros 3 ejercicios que hacen falta.

---
+++ src/RetoSeis.java
@@ -0,0 +1,24 @@
+public class RetoSeis {
+    public static void main(String[] args) {
+        int i = 0;
+
+
+        int a = 0;
+        int b = 1;
+
+        while (i <= 11) {
+            System.out.println(a);
+
+            int c = a + b;
+            a = b;
+            b= c;
+
+
+
+            i++;
+
+
+
+        }
+    }
+}
